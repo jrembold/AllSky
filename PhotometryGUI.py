7e98e894e5a333f9c95710035864a71b2088da2e
Luke Russell <lgrussel@willamette.edu>
2018-04-10 23:05:53 -0700

    made GUI pretty, switched to pack

--- PhotometryGUI.py
+++ PhotometryGUI.py
@@ -29,6 +29,7 @@ class GUI(tk.Frame):
     def __init__(self, root):
         super().__init__(root)
         self.master = root
+        self.pack(fill=BOTH, expand=True)
 
         self.tk_setPalette( background = 'black',
                             foreground = 'gray80',
@@ -39,79 +40,109 @@ class GUI(tk.Frame):
         self.createwidgets()
 
     def createwidgets(self):
+        # Frame01 = tk.Frame(self)
+        # Frame01.pack(fill=X)
 
+
+
+        # mess = r"""
+  # ______ _          _           _ _                        _           _     
+ # |  ____(_)        | |         | | |     /\               | |         (_)    
+ # | |__   _ _ __ ___| |__   __ _| | |    /  \   _ __   __ _| |_   _ ___ _ ___ 
+ # |  __| | | '__/ _ \ '_ \ / _` | | |   / /\ \ | '_ \ / _` | | | | / __| / __|
+ # | |    | | | |  __/ |_) | (_| | | |  / ____ \| | | | (_| | | |_| \__ \ \__ \
+ # |_|    |_|_|  \___|_.__/ \__,_|_|_| /_/    \_\_| |_|\__,_|_|\__, |___/_|___/
+                                                              # __/ |          
+                                                             # |___/           
+                                                             # """
+        # self.Line=tk.Label(Frame01, justify=tk.LEFT, text=mess)
+        # self.Line.pack()
+
+        ################################################## 
         Frame1 = tk.Frame(self)
-        Frame1.grid(row=0, column=0, sticky=NSEW, padx=5, pady=5)   
-
-        # self.ImageDisplay = tk.Label(Frame1, image="")
-        # self.ImageDisplay.grid(row=5,column=0)
-
-        self.CamView = []
-        self.canvas = tk.Canvas(Frame1, width = 720, height=480, bg='red')
-        self.canvas.grid(row=1,column=1)
-        self.ImageCanvas= self.canvas.create_image(0,0, anchor=NW, image=self.CamView)
-        self.canvas.bind("<Button-1>",self.ReferenceCoordinates)
-        self.canvas.bind("<Button-3>",self.ObjectCoordinates)
-
-        ThresholdSliderLength,ThresholdSliderHeight = 360,15
-        self.thresholdvar = tk.IntVar()
-        self.ThresholdSlider = tk.Scale(Frame1, from_=0, to=255, 
-                orient=HORIZONTAL, label='Threshold', sliderlength = 60, 
-                length=ThresholdSliderLength, command=self.Threshold,variable=self.thresholdvar)
-        self.ThresholdSlider.grid(row=2, column=1)
-        
-        InitialSliderLength,InitialSliderHeight = 360,15
-        self.slidervar = tk.IntVar()
-        self.InitialSlider = tk.Scale(Frame1, from_=0, to=100, 
-                orient=HORIZONTAL, label='Initial Frame', sliderlength = 60, 
-                length=InitialSliderLength, command=self.VideoLength, 
-                variable=self.slidervar)
-        self.InitialSlider.grid(row=3, column=1)
-
-        RadioWidth,RadioHeight = 13,2
-        v=tk.IntVar()
-        self.Selection = tk.Radiobutton(Frame1, text="Object", 
-                variable=v, value=0, indicatoron=0, 
-                height=RadioHeight, width=RadioWidth)
-        self.Selection2 = tk.Radiobutton(Frame1, text="Reference Star", 
-                variable=v, value=1, indicatoron=0, 
-                height=RadioHeight, width=RadioWidth)
-        self.Selection.grid(row=0, column=4)
-        self.Selection2.grid(row=0, column=5)
-
-
-        self.display = tk.Label(Frame1, image="")
-        self.display.grid(row=1,column=4)
-
-        self.LightCurve = tk.Label(Frame1, image="")
-        self.LightCurve.grid(row=1,column=2)
-
-        FrameSliderLength,FrameSliderHeight = 290,15
-        self.var = tk.IntVar()
-        self.FrameSlider = tk.Scale(Frame1, from_=1, to=2, 
-                orient=HORIZONTAL,  sliderlength = 10, 
-                length = FrameSliderLength, command=self.PlotFrame, 
-                variable=self.var)
-        self.FrameSlider.grid(row=2, column=4,
-                columnspan=FrameSliderLength, rowspan=FrameSliderHeight) 
-
+        Frame1.pack(fill=X)
+        ##################################################
 
         self.QuitButton = tk.Button(Frame1, text="QUIT", 
                 command=Frame1.quit, width=7)
-        self.QuitButton.grid(row=0,column=1)
+        self.QuitButton.pack(side=LEFT)
+
+        self.runButton = tk.Button(Frame1, text='RUN', width=7, 
+                height=1, command=self.run)
+        self.runButton.pack(side=LEFT)
+
+        self.openButton = tk.Button(Frame1, text='OPEN', width=7, 
+                height=1, command=self.open)
+        self.openButton.pack(side=LEFT)
 
         self.FolderName = tk.Entry(Frame1)
         self.FolderName.insert(0,
                 f'{datetime.datetime.now().strftime("%y-%m-%d-%H-%M")}')
-        self.FolderName.grid(row=0,column=3)
+        self.FolderName.pack(side=LEFT)
         
-        self.openButton = tk.Button(Frame1, text='OPEN', width=7, 
-                height=1, command=self.open)
-        self.openButton.grid(row=0, column=2)
+        ##################################################
+        Frame2 = tk.Frame(self)
+        Frame2.pack(fill=X) 
+        ##################################################
 
-        self.runButton = tk.Button(Frame1, text='RUN', width=7, 
-                height=1, command=self.run)
-        self.runButton.grid(row=0, column=3)
+        self.CamView = []
+        self.canvas = tk.Canvas(Frame2, width = 720, height=480, bg='black')
+        self.canvas.pack(side=LEFT)
+        self.ImageCanvas= self.canvas.create_image(0,0, anchor=NW, 
+                image=self.CamView)
+        self.canvas.bind("<Button-1>",self.ReferenceCoordinates)
+        self.canvas.bind("<Button-3>",self.ObjectCoordinates)
+
+        ##################################################
+        Frame3 = tk.Frame(self)
+        Frame3.pack(fill=X)
+        ##################################################
+
+
+        self.slidervar = tk.IntVar()
+        self.InitialSlider = tk.Scale(Frame3, from_=0, to=100, 
+                orient=HORIZONTAL, label='Initial Frame', 
+                command=self.VideoLength, variable=self.slidervar,length=360)
+        self.InitialSlider.pack(side=LEFT)
+        
+        self.thresholdvar = tk.IntVar()
+        self.ThresholdSlider = tk.Scale(Frame3, from_=0, to=255, 
+                orient=HORIZONTAL, label='Threshold',
+                command=self.Threshold,variable=self.thresholdvar,length=360)
+        self.ThresholdSlider.pack(side=LEFT)
+        
+        ##################################################
+        Frame4 = tk.Frame(self)
+        Frame4.pack(fill=X)
+        ##################################################
+       
+        self.LightCurve = tk.Label(Frame4, image="")
+        self.LightCurve.pack(side=LEFT)
+
+        self.display = tk.Label(Frame4, image="")
+        self.display.pack(side=LEFT)
+        
+        ##################################################
+        Frame5=tk.Frame(self)
+        Frame5.pack(fill=X)
+        ##################################################
+
+        self.var = tk.IntVar()
+        self.FrameSlider = tk.Scale(Frame5, from_=1, to=2, 
+                orient=HORIZONTAL, length = 360, command=self.PlotFrame, 
+                variable=self.var)
+        self.FrameSlider.pack(side=LEFT)
+
+        # RadioWidth,RadioHeight = 13,2
+        # v=tk.IntVar()
+        # self.Selection = tk.Radiobutton(Frame1, text="Object", 
+                # variable=v, value=0, indicatoron=0, 
+                # height=RadioHeight, width=RadioWidth)
+        # self.Selection2 = tk.Radiobutton(Frame1, text="Reference Star", 
+                # variable=v, value=1, indicatoron=0, 
+                # height=RadioHeight, width=RadioWidth)
+        # self.Selection.grid(row=0, column=4)
+        # self.Selection2.grid(row=0, column=5)
 
 ##################################################
 # Functions
@@ -121,10 +152,9 @@ class GUI(tk.Frame):
         FrameNumber = self.var.get()
         PlotPath= f'/home/luke/{self.FolderDirectory}/ObjectPlot{FrameNumber:03}.png'
         self.PlotImage = Image.open(PlotPath)
-        self.PlotImage = self.Original.resize((340,340),Image.ANTIALIAS)
-        self.PlotImage = ImageTk.PhotoImage(PlotImage)
+        self.PlotImage = self.PlotImage.resize((360,360),Image.ANTIALIAS)
+        self.PlotImage = ImageTk.PhotoImage(self.PlotImage)
         self.display.config(image=self.PlotImage)
-
 
     def ReferenceCoordinates(self,event):
         print(event.x,event.y)
@@ -142,11 +172,11 @@ class GUI(tk.Frame):
     
     def Threshold(self,event):
         self.ThresholdNumber = self.thresholdvar.get()
-        ThresholdView = Photometry.Threshold(self.ImageStack[:,:,self.FrameNo], self.ThresholdNumber)
+        ThresholdView = Photometry.Threshold(self.ImageStack[:,:,self.FrameNo], 
+                self.ThresholdNumber)
         self.CamView = Image.fromarray(ThresholdView)
         self.CamView = ImageTk.PhotoImage(self.CamView)
         self.canvas.itemconfig(self.ImageCanvas,image=self.CamView)
-
 
     def open(self):
         # Folder Creation
@@ -156,7 +186,6 @@ class GUI(tk.Frame):
             shutil.rmtree(FolderPath)
         os.makedirs(FolderPath)
         self.VideoName = tk.filedialog.askopenfilename(title='Choose a file')
-
         self.ImageStack = Photometry.InitialRead(self.VideoName)
         VideoSize = self.ImageStack.shape[2]
         self.InitialSlider.config(to=VideoSize-1)
@@ -165,18 +194,17 @@ class GUI(tk.Frame):
         self.CamView = ImageTk.PhotoImage(CamView)
         self.canvas.itemconfig(self.ImageCanvas,image=self.CamView)
 
-
     def run(self):
         Photometry.main(self.VideoName,self.FolderDirectory,self.FrameNo,
                 self.OBJECTLOC,self.REFERENCESTARLOC,self.ThresholdNumber)
         lightcurvepath= f'/home/luke/{self.FolderDirectory}/LightCurve.png'
         self.OG = Image.open(lightcurvepath)
-        ResizedOG = self.OG.resize((340,240),Image.ANTIALIAS)
+        ResizedOG = self.OG.resize((360,360),Image.ANTIALIAS)
         self.IMG = ImageTk.PhotoImage(ResizedOG)
-        self.LightCurve.config(image=self.IMG) 
-        MaxFrameNumber= len(glob.glob("ObjectPlot*.png"))
+        self.LightCurve.config(image=self.IMG)
+        print('1111')
+        MaxFrameNumber= len(glob.glob(f"/home/luke/{self.FolderDirectory}/ObjectPlot*.png"))
         self.FrameSlider.config(to=MaxFrameNumber)
-
 
 if __name__ == '__main__':
     GUI.main()
