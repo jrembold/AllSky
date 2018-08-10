9aa144e11c1e442065af37ed87e41137b8adcd68
Jed Rembold <jjrembold@willamette.edu>
2018-08-10 14:33:12 -0700

    Add src argument option
    Defaults to 0

--- Capture/timedRecording.py
+++ Capture/timedRecording.py
@@ -27,9 +27,10 @@ from dateutil import parser
 import cv2
 import time
 
-def initializeVideo():
+def initializeVideo(src):
     kcw = KeyClipWriter()
-    cam = VideoStream(src=0).start()
+    cam = VideoStream(src)
+    cam.start()
     return kcw, cam
 
 # def checkTimeInInterval(starttime, endtime):
@@ -59,27 +60,30 @@ def main():
     ap.add_argument('-s', '--start', help='Time and date to start recording')
     ap.add_argument('-e', '--end', help='Time and date to end recording')
     ap.add_argument('-o', '--output', help='Path to save video to')
+    ap.add_argument('--src', default=0, help='Video Source Number. Defaults to 0')
     args = vars(ap.parse_args())
 
     starttime = parser.parse(args['start'])
     endtime = parser.parse(args['end'])
+    src = int(args['src'])
     # print(f'Recording to start at: {datetime.strftime(starttime,"%Y/%m/%d %I:%M:%S")}')
     # print(f'Recording to end at:   {datetime.strftime(endtime,"%Y/%m/%d %I:%M:%S")}')
     print('Recording to start at: {}'.format(datetime.strftime(starttime,"%Y/%m/%d %I:%M:%S")))
     print('Recording to end at: {}'.format(datetime.strftime(endtime,"%Y/%m/%d %I:%M:%S")))
 
-    kcw, cam = initializeVideo()
+    kcw, cam = initializeVideo(src)
 
     #Sleep until starting time, periodically checking time
     while starttime > datetime.now():
         timetill = (starttime - datetime.now()).total_seconds()
-        frame = cam.read()
+        # frame = cam.read()
         if not round(timetill,1) % 1:
             # print(f'\rTime until recording begins: {int(timetill):5} seconds', end='', flush=True)
             print('\rTime until recording begins: {:5} seconds'.format(int(timetill)), end='', flush=True)
 
     #Wake up and start!
     print('\n---- Video recording starting! ----')
+    cam.start()
 
     while datetime.now() <= endtime:
         frame = cam.read()
@@ -87,15 +91,19 @@ def main():
 
         if not kcw.recording:
             path = "{}/{}.avi".format(args['output'], datetime.now().strftime('%Y%m%d_%H%M%S'))
-            kcw.start(path, cv2.VideoWriter_fourcc(*'FFV1'), 30)
+            kcw.start(path, cv2.VideoWriter_fourcc(*'XVID'), 30)
 
         printTimeRemaining(endtime)
+
+    cam.stop()
+    while not cam.Q.empty():
+        kcw.update(frame)
+
 
 
     #All done! Shut things down!
     print('')
     kcw.finish()
-    cam.stop()
 
 
 if __name__ == '__main__':
