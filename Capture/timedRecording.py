a42cfa8e3bd6a1bfa8486b71d1ef11183ff5d443
Jed Rembold <jjrembold@willamette.edu>
2018-08-10 15:13:52 -0700

    Fix src capture and clean up
    Reverted from using VideoStream to simplify data collection and cleaned
    up a variety of old commented out code.

--- Capture/timedRecording.py
+++ Capture/timedRecording.py
@@ -21,7 +21,6 @@ flares) for finetuning of analysis and capture software.
 
 import argparse
 from VidUtils import KeyClipWriter
-from VidUtils import VideoStream
 from datetime import datetime, timedelta
 from dateutil import parser
 import cv2
@@ -29,25 +28,18 @@ import time
 
 def initializeVideo(src):
     kcw = KeyClipWriter()
-    cam = VideoStream(src)
-    cam.start()
+    cam = cv2.VideoCapture(src)
     return kcw, cam
 
-# def checkTimeInInterval(starttime, endtime):
-    # start = parser.parse(starttime)
-    # end = parser.parse(endtime)
-    # if start < datetime.now() < end:
-        # return True
-    # else:
-        # return False
+def parseArgs():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-s', '--start', help='Time and date to start recording')
+    ap.add_argument('-e', '--end', help='Time and date to end recording')
+    ap.add_argument('-o', '--output', help='Path to save video to')
+    ap.add_argument('--src', default=0, help='Video Source Number. Defaults to 0')
+    args = vars(ap.parse_args())
+    return args
 
-# def isTimeBeforeStart(starttime):
-    # start = parser.parse(starttime)
-    # return datetime.now() < start
-
-# def isTimeAfterEnd(endtime):
-    # end = parser.parse(endtime)
-    # return datetime.now() > end
 
 def printTimeRemaining(endtime):
     timeleft = (endtime - datetime.now()).total_seconds()
@@ -56,18 +48,11 @@ def printTimeRemaining(endtime):
         print('\rTime left: {:5} seconds'.format(int(timeleft)), end='', flush=True)
 
 def main():
-    ap = argparse.ArgumentParser()
-    ap.add_argument('-s', '--start', help='Time and date to start recording')
-    ap.add_argument('-e', '--end', help='Time and date to end recording')
-    ap.add_argument('-o', '--output', help='Path to save video to')
-    ap.add_argument('--src', default=0, help='Video Source Number. Defaults to 0')
-    args = vars(ap.parse_args())
+    args = parseArgs()
 
     starttime = parser.parse(args['start'])
     endtime = parser.parse(args['end'])
     src = int(args['src'])
-    # print(f'Recording to start at: {datetime.strftime(starttime,"%Y/%m/%d %I:%M:%S")}')
-    # print(f'Recording to end at:   {datetime.strftime(endtime,"%Y/%m/%d %I:%M:%S")}')
     print('Recording to start at: {}'.format(datetime.strftime(starttime,"%Y/%m/%d %I:%M:%S")))
     print('Recording to end at: {}'.format(datetime.strftime(endtime,"%Y/%m/%d %I:%M:%S")))
 
@@ -76,34 +61,35 @@ def main():
     #Sleep until starting time, periodically checking time
     while starttime > datetime.now():
         timetill = (starttime - datetime.now()).total_seconds()
-        # frame = cam.read()
+        # frame = cam.read()[1]
         if not round(timetill,1) % 1:
-            # print(f'\rTime until recording begins: {int(timetill):5} seconds', end='', flush=True)
             print('\rTime until recording begins: {:5} seconds'.format(int(timetill)), end='', flush=True)
 
     #Wake up and start!
     print('\n---- Video recording starting! ----')
-    cam.start()
 
     while datetime.now() <= endtime:
-        frame = cam.read()
+        frame = cam.read()[1]
+        cv2.putText(frame,
+                datetime.now().strftime('%Y%m%d_%H%M%S.%f'),
+                (10, frame.shape[0]-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.35,
+                (125,125,125),
+                )
         kcw.update(frame)
 
         if not kcw.recording:
             path = "{}/{}.avi".format(args['output'], datetime.now().strftime('%Y%m%d_%H%M%S'))
-            kcw.start(path, cv2.VideoWriter_fourcc(*'XVID'), 30)
+            fourcc = cv2.VideoWriter_fourcc(*'XVID')
+            kcw.start(path, fourcc, 30)
 
         printTimeRemaining(endtime)
 
-    cam.stop()
-    while not cam.Q.empty():
-        kcw.update(frame)
-
-
-
     #All done! Shut things down!
-    print('')
-    kcw.finish()
+    print('\n---- Recording Complete ----')
+    if kcw.recording:
+        kcw.finish()
 
 
 if __name__ == '__main__':
