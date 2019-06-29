7cde15ecea1f7b1d209562846e3e7f38f4063708
Jed Rembold <jedediah.r@gmail.com>
2019-06-28 18:32:48 -0700

    Adjust detection settings to elim bugs
    Too many bugs are still showing up in the captured footage, so this is
    an attempt to mitigate that. Also, an error was found where the
    minLineLength and maxLineGap parameters were not being passed correctly
    tot he HoughLinesP function, so that was fixed.

--- Capture/motionprocess.py
+++ Capture/motionprocess.py
@@ -1,7 +1,7 @@
-#===================================================
+# ===================================================
 #
 # File Name: motionprocess.py
-# 
+#
 # Purpose: Find and record fireball events in grayscale
 #
 # Creation Date: 16-01-2017
@@ -10,7 +10,7 @@
 #
 # Created by: Jed Rembold
 #
-#===================================================
+# ===================================================
 
 import argparse
 from VidUtils import KeyClipWriter
@@ -24,72 +24,78 @@ import numpy as np
 import cv2
 import csv
 import string
+
 # import cProfile
 from threading import Thread
 from HumTemp import log_weather
 
 import shared
 
-d6log = logging.getLogger('D6')
-format_ = '[{asctime}.{msecs:<3.0f}] [{levelname:^8}]: {message}'
-fhandler = logging.FileHandler('Logs/Observation_Log.log')
-fhandler.setFormatter(logging.Formatter(format_,
-    datefmt = '%Y/%m/%d %H:%M:%S',
-    style = '{'))
+d6log = logging.getLogger("D6")
+format_ = "[{asctime}.{msecs:<3.0f}] [{levelname:^8}]: {message}"
+fhandler = logging.FileHandler("Logs/Observation_Log.log")
+fhandler.setFormatter(
+    logging.Formatter(format_, datefmt="%Y/%m/%d %H:%M:%S", style="{")
+)
 d6log.setLevel(logging.DEBUG)
 d6log.addHandler(fhandler)
 
-def next_id(prev_id:string):
-    old = base36decode(prev_id)
-    return base36encode(old+1)
 
-def base36encode(number:int):
+def next_id(prev_id: string):
+    old = base36decode(prev_id)
+    return base36encode(old + 1)
+
+
+def base36encode(number: int):
     if not isinstance(number, int):
-        raise TypeError('Number must be an integer')
+        raise TypeError("Number must be an integer")
 
     if number < 0:
-        raise ValueError('Number must be positive')
+        raise ValueError("Number must be positive")
 
     alphabet = string.digits + string.ascii_uppercase
-    base36 = ''
+    base36 = ""
 
     if 0 <= number < len(alphabet):
-        return alphabet[number].rjust(3,'0')
+        return alphabet[number].rjust(3, "0")
 
     while number != 0:
         number, i = divmod(number, len(alphabet))
         base36 = alphabet[i] + base36
 
-    return base36.rjust(3,'0')
+    return base36.rjust(3, "0")
 
-def base36decode(input:string):
+
+def base36decode(input: string):
     return int(input, 36)
 
+
 def takeSnapshot(path):
-    '''
+    """
     Function to be run in a thread to take periodic snapshots of the
     night sky. Mainly to serve as a confirmation for the sky state
     at any particular hour of the night
-    '''
+    """
     while True:
-        while (grabbed and shared.ANALYZE_ON):
+        while grabbed and shared.ANALYZE_ON:
             date_num = dt.utcnow()
             date_str = date_num.strftime("%Y%m%d_%H%M%S")
             if frame is not None:
                 savefile = frame
                 # for i in range(10):
-                    # newframe = gray
-                    # savefile = cv2.add(cv2.subtract(savefile,savefile.mean()), cv2.subtract(newframe, newframe.mean()))
+                # newframe = gray
+                # savefile = cv2.add(cv2.subtract(savefile,savefile.mean()), cv2.subtract(newframe, newframe.mean()))
                 # savefile = cv2.cvtColor(savefile, cv2.COLOR_GRAY2BGR)
-                cv2.imwrite(path + '/'+ date_str + "_Snap.png", savefile)
-                d6log.info('Taking Snapshot: {}_Snap.png'.format(date_str))
-            time.sleep(30*60) #Sleep 30 mins
-        time.sleep(5*60) #If analysis off, wait 10 minutes then check again
+                cv2.imwrite(path + "/" + date_str + "_Snap.png", savefile)
+                d6log.info("Taking Snapshot: {}_Snap.png".format(date_str))
+            time.sleep(30 * 60)  # Sleep 30 mins
+        time.sleep(5 * 60)  # If analysis off, wait 10 minutes then check again
 
-def weightaccum( old_frame, new_frame, weight ):
-    ''' Function calculates an accumulated weighted average. This function
+
+def weightaccum(old_frame, new_frame, weight):
+    """ Function calculates an accumulated weighted average. This function
     serves as a wrapper to the basic script to recast the arrays in the
-    needed datatype. '''
+    needed datatype. """
     # Recasting in float32 as accumulateWeighted requires this datatype
     old_frame = old_frame.astype(np.float32)
     new_frame = new_frame.astype(np.float32)
@@ -99,46 +105,46 @@ def weightaccum( old_frame, new_frame, weight ):
     return old_frame.astype(np.uint8)
 
 
-def drawBoundingHough(frame, x1, x2, y1, y2 ):
-    ''' Function to draw a red box around all detected objects in a scene
-    '''
+def drawBoundingHough(frame, x1, x2, y1, y2):
+    """ Function to draw a red box around all detected objects in a scene
+    """
     size = 40
-    midx = int((x2+x1)/2)
-    midy = int((y2+y1)/2)
-    p1x = max(midx-size,0)
-    p1y = max(midy-size,0)
-    p2x = min(midx+size, frame.shape[1])
-    p2y = min(midy+size, frame.shape[0])
-    cv2.rectangle(frame, (p1x, p1y), (p2x,p2y), 255, 1)
+    midx = int((x2 + x1) / 2)
+    midy = int((y2 + y1) / 2)
+    p1x = max(midx - size, 0)
+    p1y = max(midy - size, 0)
+    p2x = min(midx + size, frame.shape[1])
+    p2y = min(midy + size, frame.shape[0])
+    cv2.rectangle(frame, (p1x, p1y), (p2x, p2y), 255, 1)
 
 
 def get_Hough_Avg_Pt(lines):
     x1, y1, x2, y2 = lines[0]
-    return ( int((x1+x2)/2), int((y1+y2)/2) )
+    return (int((x1 + x2) / 2), int((y1 + y2) / 2))
 
 
-def write_to_table(pos:'(x,y)'):
-    with open('Logs/Obs_Table.csv', 'r') as f:
+def write_to_table(pos: "(x,y)"):
+    with open("Logs/Obs_Table.csv", "r") as f:
         row = next(reversed(list(csv.reader(f))))
         code = row[0]
 
     params = {
-            'code': next_id(code),
-            'local': dt.strftime(dt.now(), "%Y%m%d %H:%M:%S"),
-            'utc': dt.strftime(dt.utcnow(), "%Y%m%d %H:%M:%S"),
-            'ptx': pos[0],
-            'pty': pos[1],
-            }
+        "code": next_id(code),
+        "local": dt.strftime(dt.now(), "%Y%m%d %H:%M:%S"),
+        "utc": dt.strftime(dt.utcnow(), "%Y%m%d %H:%M:%S"),
+        "ptx": pos[0],
+        "pty": pos[1],
+    }
 
-    with open('Logs/Obs_Table.csv', 'a') as f:
-        f.write('{code},{local},{utc},{ptx},{pty}\n'.format(**params))
+    with open("Logs/Obs_Table.csv", "a") as f:
+        f.write("{code},{local},{utc},{ptx},{pty}\n".format(**params))
 
 
-def analyze(buffsize, savepath, headless, vpath=None ):
-    '''
+def analyze(buffsize, savepath, headless, vpath=None):
+    """
     This is the main analysis script. Reads in the data from the camera,
     checks for objects, opens threads to save the data if an object is
-    found. '''
+    found. """
 
     global grabbed, gray, frame
 
@@ -158,13 +164,20 @@ def analyze(buffsize, savepath, headless, vpath=None ):
     kcw = KeyClipWriter(bufSize=buffsize)
     consecFrames = 0
     d6log.info("New Observation Run Started")
-    d6log.info("Detection params -- Length: {}, Threshold: {}, MinLine: {}, LineSkip: {}".format(shared.DETECT.LENGTH, shared.DETECT.THRESHOLD, shared.DETECT.MINLINE, shared.DETECT.LINESKIP))
+    d6log.info(
+        "Detection params -- Length: {}, Threshold: {}, MinLine: {}, LineSkip: {}".format(
+            shared.DETECT.LENGTH,
+            shared.DETECT.THRESHOLD,
+            shared.DETECT.MINLINE,
+            shared.DETECT.LINESKIP,
+        )
+    )
     d6log.info("Clips saved to {}".format(savepath))
 
     disk_full = False
     vidFrames = 0
 
-    # Grab one frame to initialize 
+    # Grab one frame to initialize
     (grabbed, frame) = cam.read()
 
     # Start snapshoting thread
@@ -173,10 +186,9 @@ def analyze(buffsize, savepath, headless, vpath=None ):
     snapshots.start()
 
     # Start weather logging thread
-    weather = Thread(target=log_weather, args=(5*60,))
+    weather = Thread(target=log_weather, args=(5 * 60,))
     weather.daemon = True
     weather.start()
-
 
     while grabbed:
         # Check the time to see what hour of the day it is
@@ -213,66 +225,82 @@ def analyze(buffsize, savepath, headless, vpath=None ):
             gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
             B = gray.copy()
 
-
             # Resize grayscale image for faster image processing (reducing each dimension by a factor of 2)
             gray = cv2.resize(gray, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
 
             # Average Frame
             if avg is None:
                 avg = gray.copy()
-            # Using a very small weighted average to vastly prefer older 
-            # frames that do not include any current fireballs, 
+            # Using a very small weighted average to vastly prefer older
+            # frames that do not include any current fireballs,
             # thereby creating our background
-            avg = weightaccum( avg, gray, 0.05 )
+            avg = weightaccum(avg, gray, 0.05)
 
             # Subtract and Threshold
             delta = cv2.subtract(gray, avg)
             thresh = cv2.threshold(delta, 30, 255, cv2.THRESH_BINARY)[1]
-            kernel = np.ones([3,3])
+            kernel = np.ones([3, 3])
             thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
 
             # Accumulated binaries to create line for Hough transform to find
             if accum is None:
                 accum = thresh.copy()
-            # Using a larger weighted average to create a composite of the 
+            # Using a larger weighted average to create a composite of the
             # more recent frames, which should include the fireball trail
-            accum = weightaccum( accum, thresh, 0.1 )
+            accum = weightaccum(accum, thresh, 0.1)
             # Thresholding the accumulated image for Hough Transforming
             accum_thresh = cv2.threshold(accum, 10, 255, cv2.THRESH_BINARY)[1]
 
             # Writing date and time in UTC to lower left corner of output frame in green
             date_num = dt.utcnow()
             date_str = date_num.strftime("%Y%m%d %H%M%S.%f")
-            cv2.putText(B, date_str + " UTC", (10, B.shape[0]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.35, 255)
+            cv2.putText(
+                B,
+                date_str + " UTC",
+                (10, B.shape[0] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.35,
+                255,
+            )
 
             # The Hough Transform
-            lines = cv2.HoughLinesP( accum_thresh, shared.DETECT.LENGTH, shared.DETECT.ANGLES, shared.DETECT.THRESHOLD, shared.DETECT.MINLINE, shared.DETECT.LINESKIP )
+            lines = cv2.HoughLinesP(
+                accum_thresh,
+                rho=shared.DETECT.LENGTH,
+                theta=shared.DETECT.ANGLES,
+                threshold=shared.DETECT.THRESHOLD,
+                minLineLength=shared.DETECT.MINLINE,
+                maxLineGap=shared.DETECT.LINESKIP,
+            )
             # If we detect lines, draw a box around each one and initialize or perpetuate recording
-            if lines is not None and len(lines)<50:
+            if lines is not None and len(lines) < 50:
                 # for eachline in lines:
-                    # for x1, y1, x2, y2 in eachline:
-                        # Bounding box edges multiplied by 2 to account for dimension reduction earlier
-                        #drawBoundingHough(R, 2*x1, 2*x2, 2*y1, 2*y2)
+                # for x1, y1, x2, y2 in eachline:
+                # Bounding box edges multiplied by 2 to account for dimension reduction earlier
+                # drawBoundingHough(R, 2*x1, 2*x2, 2*y1, 2*y2)
                 updateConsecFrames = False
                 consecFrames = 0
-
 
                 # If not already recording, start the recording!
                 if not kcw.recording:
                     # Get the current free space on the disk in megabytes
-                    free_space = shutil.disk_usage(savepath).free*1E-6
+                    free_space = shutil.disk_usage(savepath).free * 1e-6
                     # If we have more than 500MB available, go ahead and start the recording, else stop program
                     if free_space > 500:
                         # print("New event found at time: {}".format(date_str))
-                        p = "{}/{}.avi".format(savepath, date_num.strftime("%Y%m%d_%H%M%S"))
+                        p = "{}/{}.avi".format(
+                            savepath, date_num.strftime("%Y%m%d_%H%M%S")
+                        )
                         # kcw.start(p, cv2.VideoWriter_fourcc(*'H264'), 30)
                         # kcw.start(p, cv2.VideoWriter_fourcc(*'FFV1'), 30)
-                        kcw.start(p, cv2.VideoWriter_fourcc(*'XVID'), 30)
+                        kcw.start(p, cv2.VideoWriter_fourcc(*"XVID"), 30)
                         # kcw.start(p, cv2.VideoWriter_fourcc(*'HFYU'), 30)
                         d6log.info("New event detected. Video name: {}".format(p))
                         write_to_table(get_Hough_Avg_Pt(lines[0]))
                     else:
-                        d6log.warning("Terminating observation run due to lack of storage")
+                        d6log.warning(
+                            "Terminating observation run due to lack of storage"
+                        )
                         disk_full = True
                         shared.ANALYZE_ON = False
 
@@ -280,7 +308,7 @@ def analyze(buffsize, savepath, headless, vpath=None ):
             # output = cv2.merge([B, G, R])
             output = cv2.cvtColor(B, cv2.COLOR_GRAY2RGB)
 
-            #Wrapping things up
+            # Wrapping things up
             if updateConsecFrames:
                 consecFrames += 1
                 vidFrames = 0
@@ -288,16 +316,17 @@ def analyze(buffsize, savepath, headless, vpath=None ):
                 vidFrames += 1
                 if vidFrames > 100:
                     kcw.terminate()
-                    d6log.warning("Event was too long and was terminated early as false positive.")
+                    d6log.warning(
+                        "Event was too long and was terminated early as false positive."
+                    )
 
-
-            #Update buffer with latest frame
+            # Update buffer with latest frame
             kcw.update(output)
 
-            #If too many frames w/o an event, stop recording
+            # If too many frames w/o an event, stop recording
             if kcw.recording and consecFrames == buffsize:
                 kcw.finish()
-                d6log.info("Event completed and video recording finished") 
+                d6log.info("Event completed and video recording finished")
 
             # Show windows if desired
             if not headless:
@@ -305,9 +334,9 @@ def analyze(buffsize, savepath, headless, vpath=None ):
                 # cv2.imshow("Timestamp",G)
                 # cv2.imshow("Box", R)
                 cv2.imshow("Background", avg)
-                cv2.imshow("Subtracted",delta)
+                cv2.imshow("Subtracted", delta)
                 cv2.imshow("Accumulated", accum_thresh)
-                
+
                 key = cv2.waitKey(1)
 
                 # Exit script early. This does not work if running in headless mode
@@ -320,7 +349,7 @@ def analyze(buffsize, savepath, headless, vpath=None ):
 
             # Occasionally print out the current framerate
             if not framenum % 5:
-                shared.FRAMERATE = 1/(endframetime-startframetime)
+                shared.FRAMERATE = 1 / (endframetime - startframetime)
                 # print("Operating at {} frames per second".format(1/(endframetime-startframetime)))
 
             # Check time
@@ -335,7 +364,7 @@ def analyze(buffsize, savepath, headless, vpath=None ):
             kcw.finish()
 
         # if key == ord("q"):
-            # break
+        # break
 
         if not shared.RUNNING:
             shared.ANALYZE_ON = False
@@ -347,18 +376,19 @@ def analyze(buffsize, savepath, headless, vpath=None ):
 
     d6log.info("Observing session finished.")
 
+
 if __name__ == "__main__":
-    #Setting up logging formatting and location
+    # Setting up logging formatting and location
     logging.basicConfig(
-            #Logging to Log.txt in same directory as script
-            filename = 'Logs/Observation_Log.log',
-            level = logging.DEBUG,
-            style = '{',
-            format = '[{asctime}.{msecs:<3.0f}] [{levelname:^8}]: {message}',
-            # datefmt = '%H:%M:%S',
-            datefmt = '%Y/%m/%d %H:%M:%S',
-            # filemode = 'w',
-            )
+        # Logging to Log.txt in same directory as script
+        filename="Logs/Observation_Log.log",
+        level=logging.DEBUG,
+        style="{",
+        format="[{asctime}.{msecs:<3.0f}] [{levelname:^8}]: {message}",
+        # datefmt = '%H:%M:%S',
+        datefmt="%Y/%m/%d %H:%M:%S",
+        # filemode = 'w',
+    )
 
     shared.RUNNING = True
     grabbed = False
@@ -370,11 +400,22 @@ if __name__ == "__main__":
     # Construct the Argument Parser and Parse away!
     ap = argparse.ArgumentParser()
     ap.add_argument("-v", "--video", help="Path to desired video file")
-    ap.add_argument("-b", "--buffer-size", type=int, default=32, help="Buffer size of video clip writer (defaults to 32)")
+    ap.add_argument(
+        "-b",
+        "--buffer-size",
+        type=int,
+        default=32,
+        help="Buffer size of video clip writer (defaults to 32)",
+    )
     ap.add_argument("-o", "--output", help="Path to desired output files")
-    ap.add_argument("-n", "--headless", action='store_true', help="Pass option to suppress monitoring output")
+    ap.add_argument(
+        "-n",
+        "--headless",
+        action="store_true",
+        help="Pass option to suppress monitoring output",
+    )
     args = vars(ap.parse_args())
     # print(args)
 
     # cProfile.run('main()')
-    analyze(args['buffer_size'], args['output'], args['headless'],args['video'])
+    analyze(args["buffer_size"], args["output"], args["headless"], args["video"])
--- Capture/videodetect.py
+++ Capture/videodetect.py
@@ -8,7 +8,21 @@ import numpy as np
 import cv2
 
 import shared
-import motionprocess as mp
+
+# import motionprocess as mp
+
+
+def weightaccum(old_frame, new_frame, weight):
+    """ Function calculates an accumulated weighted average. This function
+    serves as a wrapper to the basic script to recast the arrays in the
+    needed datatype. """
+    # Recasting in float32 as accumulateWeighted requires this datatype
+    old_frame = old_frame.astype(np.float32)
+    new_frame = new_frame.astype(np.float32)
+    # Doing the weighted average
+    cv2.accumulateWeighted(new_frame, old_frame, weight)
+    # Returning the result cast back in uint8
+    return old_frame.astype(np.uint8)
 
 
 def detect(frame, avg, accum):
@@ -17,96 +31,98 @@ def detect(frame, avg, accum):
 
     # Resize grayscale image for faster image processing (reducing each dimension by a factor of 2)
     gray = cv2.resize(gray, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-    
+
     # Average Frame
     if avg is None:
         avg = gray.copy()
-    # Using a very small weighted average to vastly prefer older 
-    # frames that do not include any current fireballs, 
+    # Using a very small weighted average to vastly prefer older
+    # frames that do not include any current fireballs,
     # thereby creating our background
-    avg = mp.weightaccum( avg, gray, 0.01 )
+    avg = weightaccum(avg, gray, 0.01)
 
     # Subtract and Threshold
     delta = cv2.subtract(gray, avg)
-    cv2.imshow('Delta', delta)
+    cv2.imshow("Delta", delta)
     thresh = cv2.threshold(delta, 20, 255, cv2.THRESH_BINARY)[1]
     # kernel = np.ones([3,3])
-    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2,2))
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS, (2, 2))
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-    cv2.imshow('Thresh', thresh)
+    cv2.imshow("Thresh", thresh)
 
     # Accumulated binaries to create line for Hough transform to find
     if accum is None:
         accum = thresh.copy()
-    # Using a larger weighted average to create a composite of the 
+    # Using a larger weighted average to create a composite of the
     # more recent frames, which should include the fireball trail
-    accum = mp.weightaccum( accum, thresh, 0.1 )
+    accum = weightaccum(accum, thresh, 0.2)
     # Thresholding the accumulated image for Hough Transforming
     accum_thresh = cv2.threshold(accum, 10, 255, cv2.THRESH_BINARY)[1]
     # The Hough Transform
     lines = cv2.HoughLinesP(
-            accum_thresh,
-            shared.DETECT.LENGTH,
-            shared.DETECT.ANGLES,
-            shared.DETECT.THRESHOLD,
-            shared.DETECT.MINLINE,
-            shared.DETECT.LINESKIP )
+        accum_thresh,
+        rho=shared.DETECT.LENGTH,
+        theta=shared.DETECT.ANGLES,
+        threshold=shared.DETECT.THRESHOLD,
+        minLineLength=shared.DETECT.MINLINE,
+        maxLineGap=shared.DETECT.LINESKIP,
+    )
     # If we detect lines, draw a box around each one and initialize or perpetuate recording
     marked = frame.copy()
     avgpt = None
     if lines is not None:
         for eachline in lines:
             for x1, y1, x2, y2 in eachline:
-                cv2.line(frame, (x1*2,y1*2), (x2*2,y2*2), (0,0,255))
-        x1, y1, x2, y2 = lines[0,0]
-        avgpt = ((x1+x2)/2, (y1+y2)/2 )
+                cv2.line(frame, (x1 * 2, y1 * 2), (x2 * 2, y2 * 2), (0, 0, 255))
+        x1, y1, x2, y2 = lines[0, 0]
+        avgpt = ((x1 + x2) / 2, (y1 + y2) / 2)
         linefound = True
     else:
         linefound = False
 
     return linefound, avg, accum, accum_thresh, marked, avgpt
 
+
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument('-v', '--video', help='Path to desired video file')
+    ap.add_argument("-v", "--video", help="Path to desired video file")
     args = vars(ap.parse_args())
 
-    vid = cv2.VideoCapture(args['video'])
+    vid = cv2.VideoCapture(args["video"])
     grabbed, frame = vid.read()
 
     avg = None
     accum = None
 
-    cv2.namedWindow('Frame')
-    cv2.moveWindow('Frame', 100, 400)
-    cv2.namedWindow('Background')
-    cv2.moveWindow('Background', 95, 150)
-    cv2.namedWindow('Delta')
-    cv2.moveWindow('Delta', 105 + 360, 150)
+    cv2.namedWindow("Frame")
+    cv2.moveWindow("Frame", 100, 400)
+    cv2.namedWindow("Background")
+    cv2.moveWindow("Background", 95, 150)
+    cv2.namedWindow("Delta")
+    cv2.moveWindow("Delta", 105 + 360, 150)
 
-    cv2.namedWindow('Marked')
-    cv2.moveWindow('Marked', 900, 400)
-    cv2.namedWindow('Thresh')
-    cv2.moveWindow('Thresh', 895, 150)
-    cv2.namedWindow('Accumulated')
-    cv2.moveWindow('Accumulated', 905 + 360, 150)
+    cv2.namedWindow("Marked")
+    cv2.moveWindow("Marked", 900, 400)
+    cv2.namedWindow("Thresh")
+    cv2.moveWindow("Thresh", 895, 150)
+    cv2.namedWindow("Accumulated")
+    cv2.moveWindow("Accumulated", 905 + 360, 150)
 
     while grabbed:
         linefound, avg, accum, accum_thresh, marked, avgpt = detect(frame, avg, accum)
 
-        cv2.imshow('Frame', frame)
-        cv2.imshow('Background', avg)
-        cv2.imshow('Accumulated', accum_thresh)
-        cv2.imshow('Marked', marked)
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Background", avg)
+        cv2.imshow("Accumulated", accum_thresh)
+        cv2.imshow("Marked", marked)
         if linefound:
             print(avgpt)
 
         key = cv2.waitKey()
-        if key == ord('q'):
+        if key == ord("q"):
             break
 
         grabbed, frame = vid.read()
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
