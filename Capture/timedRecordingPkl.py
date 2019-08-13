33181086ad153f3c786b07e274dcb006e01873b5
Jed Rembold <jjrembold@willamette.edu>
2019-08-12 22:47:29 -0700

    Add pickle version of saved video

---
+++ Capture/timedRecordingPkl.py
@@ -0,0 +1,97 @@
+#===================================================
+#
+# File Name: timedRecording.py
+# 
+# Purpose: To record footage during a set time interval 
+#
+# Creation Date: 05-06-2017
+#
+# Last Modified: Thu 31 May 2018 05:26:32 PM PDT
+#
+# Created by: Jed Rembold
+#
+#===================================================
+
+'''
+Script should check the local clock to a set time interval and
+if within that time interval, should record. Written with the
+intention of being used to capture known events (Iridium
+flares) for finetuning of analysis and capture software.
+'''
+
+import argparse
+from VidUtils import KeyClipWriter
+from datetime import datetime, timedelta
+from dateutil import parser
+import cv2
+import time
+from collections import deque
+import pickle
+
+def initializeVideo(src):
+    cam = cv2.VideoCapture(src)
+    Q = deque()
+    return cam, Q
+
+def parseArgs():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-s', '--start', help='Time and date to start recording')
+    ap.add_argument('-e', '--end', help='Time and date to end recording')
+    ap.add_argument('-o', '--output', help='Path to save video to')
+    ap.add_argument('--src', default=0, help='Video Source Number. Defaults to 0')
+    args = vars(ap.parse_args())
+    return args
+
+
+def printTimeRemaining(endtime):
+    timeleft = (endtime - datetime.now()).total_seconds()
+    if not round(timeleft,1) % 1:
+        # print(f'\rRecording Time Left: {int(timeleft):5} seconds', end='', flush=True)
+        print('\rTime left: {:5} seconds'.format(int(timeleft)), end='', flush=True)
+
+def main():
+    args = parseArgs()
+
+    starttime = parser.parse(args['start'])
+    endtime = parser.parse(args['end'])
+    src = int(args['src'])
+    print('Recording to start at: {}'.format(datetime.strftime(starttime,"%Y/%m/%d %I:%M:%S")))
+    print('Recording to end at: {}'.format(datetime.strftime(endtime,"%Y/%m/%d %I:%M:%S")))
+
+    cam, Q = initializeVideo(src)
+
+    #Sleep until starting time, periodically checking time
+    while starttime > datetime.now():
+        timetill = (starttime - datetime.now()).total_seconds()
+        # frame = cam.read()[1]
+        if not round(timetill,1) % 1:
+            print('\rTime until recording begins: {:5} seconds'.format(int(timetill)), end='', flush=True)
+
+    #Wake up and start!
+    print('\n---- Video recording starting! ----')
+
+    while datetime.now() <= endtime:
+        frame = cam.read()[1]
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        cv2.putText(frame,
+                datetime.now().strftime('%Y%m%d_%H%M%S.%f'),
+                (10, frame.shape[0]-10),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                0.35,
+                125,
+                )
+        Q.append(frame)
+
+        printTimeRemaining(endtime)
+
+    #All done! Shut things down!
+    print('\n---- Recording Complete ----')
+    path = "{}/{}.pkl".format(args['output'], datetime.now().strftime('%Y%m%d_%H%M%S'))
+    with open(path, 'wb') as f:
+        pickle.dump(Q, f)
+
+if __name__ == '__main__':
+    main()
+    
+
+
